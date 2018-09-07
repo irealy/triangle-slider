95f4cce66b4adf823c622f7f57993aa7516dd470
Богдан Кононов <b.b.kononov@gmail.com>
2018-09-07 14:47:23 +0300

    update for ssafari

--- src/js/app.js
+++ src/js/app.js
@@ -9,14 +9,14 @@ $('body').on('click',() => {
     .to(state,2,{
     	pos:1,
     	onUpdate: function() {
-    		let perc = '-'+state.pos*100+'%';
-    		let perc1 = '-'+state.pos*state.pos*100+'%';
-    		let perc2 = '-'+state.pos*state.pos*state.pos*100+'%';
+    		let perc = '-'+state.pos*900;
+    		let perc1 = '-'+state.pos*state.pos*900;
+    		let perc2 = '-'+state.pos*state.pos*state.pos*900;
     		// let perc = '-'+state.pos*100+'%';
     		tl1
-    		.set('.slider__1 .slider__wrap',{x:perc},0)
-    		.set('.slider__2 .slider__wrap',{x:perc1},0)
-    		.set('.slider__3 .slider__wrap',{x:perc2},0);
+          .set('.slider__1 .slider__wrap',{x:perc},0)
+          .set('.slider__2 .slider__wrap',{x:perc1},0)
+          .set('.slider__3 .slider__wrap',{x:perc2},0);
     	}
     });
 });
